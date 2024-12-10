9a613c1189e3abd22b4e152220764d47cf863e97
ibrahimhalilçadak <ibrahimhalilcadak@gmail.com>
2024-12-10 21:28:21 +0300

    first commit

---
+++ src/utils/CheckValid.js
@@ -0,0 +1,8 @@
+
+// Eğer ki parametre olarak aldığı değer tanımlıysa o değeri geri  döndüren ama tanımsız ise "Bilinmiyor" metni döndüren fonksiyon
+const checkValid = (value) => {
+    return !value ? "Bilinmiyor" : value;
+  };
+  
+  export default checkValid;
+  
